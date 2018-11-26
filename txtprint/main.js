977ef52b3965fa8ca1324dcc76fb769b58e0773e
rydergibson <rydergibson1227@gmail.com>
2018-11-26 08:55:09 -0800

    Add files via upload

---
+++ txtprint/main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+   // document.getElementsByTagName('style')[0].outerHTML = "";
+   var typed = new Typed('#header', {
+    strings: ['Print <b>conclusions</b>', 'Print <b>essays</b>', 'Print <b>sentences</b>','Print <b>anything</b>^750'],
+    typeSpeed: 45,
+    backSpeed: 45,
+    smartBackspace: true,
+    loop: false,
+    showCursor: false,
+});
+}
